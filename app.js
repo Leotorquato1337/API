fd1aee33d157409805d0653ca83756323e4420a4
Leotorquato1337 <leeofernaandes@gmail.com>
2025-05-19 21:02:40 -0300

    API FUNCIONANDO

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('API FUNCIONANDO');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
